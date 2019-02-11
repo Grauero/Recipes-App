fd190170c674f2f07a5a08e2605e075d673e854c
Grauero <grayero6996@gmail.com>
2019-02-11 23:04:55 +0300

    mend

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true
+  },
+  rules: {
+    'comma-dangle': 0,
+    'linebreak-style': 0,
+    'object-curly-newline': 0,
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    'global-require': 0
+  }
+};
